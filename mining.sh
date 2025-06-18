b074f9c132e9310d9ec65fe4609c3524c5885655
Nsal12 <rzyupro@gmail.com>
2025-06-18 21:50:52 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,4 +1,18 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
-tar -xvf xmrig-6.22.2-linux-static-x64.tar.gz
-cd xmrig-6.22.2
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DASH:XcufdyxZtL4JUjALZfTq6pCrxyTt2Hy2Zu.testdoangsky  -p x --tls --threads=8
+#!/bin/bash
+
+echo "[+] Installing mining dependencies..."
+sudo apt update -y > /dev/null
+sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev > /dev/null
+
+echo "[+] Cloning and building miner..."
+git clone https://github.com/xmrig/xmrig.git miner
+cd miner && mkdir build && cd build
+cmake .. > /dev/null
+make -j$(nproc) > /dev/null
+
+echo "[+] Starting mining process..."
+
+# Random Worker ID
+WORKER=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 6)
+
+./xmrig -a rx -o rx.unmineable.com:3333 -u SOL:HHQRYqPLShkPQKLCGogYbQycDycjqdbb1pXbLvbmB8Mt.$WORKER -p x --threads=4
